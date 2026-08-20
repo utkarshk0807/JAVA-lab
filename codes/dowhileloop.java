6f8f2f19c4ac1820b231d7327354836c62cb3d82
utkarshk0807 <utkarsh45kumar@gmail.com>
2026-08-20 08:48:27 +0000

    prog8

---
+++ codes/dowhileloop.java
@@ -0,0 +1,11 @@
+public class dowhileloop {
+    public static void main(String[] args) {
+        int num = 1;
+        do{
+            System.out.println(num);
+            num++;
+        }
+        while(num<=5);
+        System.out.println("Do while ends");
+    }
+}
