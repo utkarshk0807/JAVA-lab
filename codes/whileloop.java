1689af608ca7c73768f309cd2d8b309e4be7090d
utkarshk0807 <utkarsh45kumar@gmail.com>
2026-08-20 08:42:26 +0000

    prog7

---
+++ codes/whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[] args){
+        int num = 1;
+        while(num<=5){
+            System.out.println("Iteration: "+num);
+            num++;
+        }
+        System.out.println("Loop Ends");
+    }
+}
