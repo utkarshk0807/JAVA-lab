849d77261e4a61dbacd4422b7a9a8db062085902
utkarshk0807 <utkarsh45kumar@gmail.com>
2026-08-20 09:25:08 +0000

    prog10

---
+++ codes/breakstatement.java
@@ -0,0 +1,12 @@
+public class breakstatement {
+    public static void main(String[] args) {
+        int i;
+        i =1;
+        while(true){
+            if(i>10)
+                break;
+            System.out.println(i+" ");
+            i++;
+        }
+    }
+}
